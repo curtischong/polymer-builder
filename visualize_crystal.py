1bc8d080026bdd5cc925cad42ce124da1662ec91
Curtis Chong <curtischong5@gmail.com>
2024-09-07 19:23:54 +0000

    mor work on plotting the material

--- visualize_crystal.py
+++ visualize_crystal.py
@@ -12,47 +12,33 @@ class VisualizationSetting(Enum):
     ALL_DETAILED = 3
 
 
-def element_color(symbol):
+def element_color(atomic_number: int) -> str:
     # Dictionary mapping chemical symbols to colors
     color_map = {
-        "H": "#F0F8FF",  # Hydrogen
-        "He": "#D3D3D3",  # Helium
-        "Li": "#B22222",  # Lithium
-        "Be": "#9ACD32",  # Beryllium
-        "B": "#FFD700",  # Boron
-        "C": "#000000",  # Carbon
-        "N": "#8F00FF",  # Nitrogen
-        "O": "#FF0000",  # Oxygen
-        "F": "#DAA520",  # Fluorine
-        "Ne": "#EE82EE",  # Neon
-        "Na": "#0000FF",  # Sodium
-        "Mg": "#228B22",  # Magnesium
-        "Al": "#C0C0C0",  # Aluminum
-        "Si": "#A52A2A",  # Silicon
-        "P": "#FFA500",  # Phosphorus
-        "S": "#FFFF00",  # Sulfur
-        "Cl": "#00FF00",  # Chlorine
-        "Ar": "#FF00FF",  # Argon
-        "K": "#F0E68C",  # Potassium
-        "Ca": "#E6E6FA",  # Calcium
+        1: "#F0F8FF",  # Hydrogen
+        2: "#D3D3D3",  # Helium
+        3: "#B22222",  # Lithium
+        4: "#9ACD32",  # Beryllium
+        5: "#FFD700",  # Boron
+        6: "#000000",  # Carbon
+        7: "#8F00FF",  # Nitrogen
+        8: "#FF0000",  # Oxygen
+        9: "#DAA520",  # Fluorine
+        10: "#EE82EE",  # Neon
+        11: "#0000FF",  # Sodium
+        12: "#228B22",  # Magnesium
+        13: "#C0C0C0",  # Aluminum
+        14: "#A52A2A",  # Silicon
+        15: "#FFA500",  # Phosphorus
+        16: "#FFFF00",  # Sulfur
+        17: "#00FF00",  # Chlorine
+        18: "#FF00FF",  # Argon
+        19: "#F0E68C",  # Potassium
+        20: "#E6E6FA",  # Calcium
     }
 
     # Return the color for the given chemical symbol, or a default color if not found
-    return color_map.get(symbol, "#808080")  # Default color is gray
-
-
-def plot_bonds(fig, structure: Structure):
-    bonds = predict_bonds(structure)
-    plot_edges(fig, bonds, "#303030")
-
-
-def vis_crystal_during_sampling(
-    A: np.ndarray,
-    frac_x: np.ndarray,
-    name: str,
-):
-    atomic_numbers = atomic_number_indexes_to_atomic_numbers(z_table, A)
-    return visualize_and_save_crystal(atomic_numbers, frac_x, name)
+    return color_map.get(atomic_number, "#808080")  # Default color is gray
 
 
 def visualize_and_save_crystal(
@@ -68,33 +54,34 @@ def visualize_and_save_crystal(
 
 def plot_crystal(
     atomic_numbers: np.ndarray,
-    frac_x: np.ndarray,
+    coords: np.ndarray,
 ) -> go.Figure:
     # we use min(z, 118) since the mask state needs a visualization
     element_symbols = [Element.from_Z(min(z, 118)).symbol for z in atomic_numbers]
     pos_arr = []
     for i in range(len(atomic_numbers)):
-        pos_arr.append(frac_x[i].tolist())
+        pos_arr.append(coords[i])
 
     # Create a Plotly figure
     fig = go.Figure()
 
     # Add traces for each atom in the structure
-    for site in structure.sites:
-        atom_type = str(site.specie)
+    for i in range(len(pos_arr)):
+        coord = pos_arr[i]
+        atomic_num = atomic_numbers[i]
         fig.add_trace(
             go.Scatter3d(
-                x=[site.x],
-                y=[site.y],
-                z=[site.z],
+                x=[coord[0]],
+                y=[coord[1]],
+                z=[coord[2]],
                 mode="markers",
                 marker=dict(
                     size=5,
                     color=element_color(
-                        atom_type
+                        atomic_num
                     ),  # Set the color based on the atom type
                 ),
-                name=atom_type,
+                name=element_symbols[i],
             )
         )
 
