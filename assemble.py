83abfa3d03bdca5d0c58147e73eda6f6fbe6693b
Curtis Chong <curtischong5@gmail.com>
2024-09-08 21:46:26 +0000

    move existing code to contained func

--- assemble.py
+++ assemble.py
@@ -3,43 +3,47 @@ import json
 from sevenn_runner import SevenNetCalculator
 
 
-sevennet_0_cal = SevenNetCalculator("7net-0", device="auto")  # 7net-0, SevenNet-0, 7net-0_22May2024, 7net-0_11July2024 ...
-print(f"running on device {sevennet_0_cal.device}")
-smiles="NCC(=O)NCCCCCC(=O)"
-atomic_nums, coords_log, last_non_hydrogen_idx_on_main_chain = get_molecules.grow_two_molecules(sevennet_0_cal, smiles)
+def create_single_chain():
+    sevennet_0_cal = SevenNetCalculator("7net-0", device="auto")  # 7net-0, SevenNet-0, 7net-0_22May2024, 7net-0_11July2024 ...
+    print(f"running on device {sevennet_0_cal.device}")
+    smiles="NCC(=O)NCCCCCC(=O)"
+    atomic_nums, coords_log, last_non_hydrogen_idx_on_main_chain = get_molecules.grow_two_molecules(sevennet_0_cal, smiles)
 
-relax_batches = [{
-    "atomic_nums": atomic_nums.tolist(),
-    "relax_len": len(coords_log),
-}]
-
-num_monomers = 5
-for i in range(num_monomers - 2): # -2 since we already have the first 2 monomers
-    atomic_nums, coords_log, last_non_hydrogen_idx_on_main_chain = get_molecules.grow_on_chain(sevennet_0_cal, relax_batches[-1]["atomic_nums"], coords_log, last_non_hydrogen_idx_on_main_chain, smiles)
-    relax_batches.append({
+    relax_batches = [{
         "atomic_nums": atomic_nums.tolist(),
+        "relax_len": len(coords_log),
+    }]
+
+    num_monomers = 5
+    for i in range(num_monomers - 2): # -2 since we already have the first 2 monomers
+        atomic_nums, coords_log, last_non_hydrogen_idx_on_main_chain = get_molecules.grow_on_chain(sevennet_0_cal, relax_batches[-1]["atomic_nums"], coords_log, last_non_hydrogen_idx_on_main_chain, smiles)
+        relax_batches.append({
+            "atomic_nums": atomic_nums.tolist(),
+            "relax_len": len(coords_log),
+        })
+
+    coords_log = coords_log + get_molecules.relax(sevennet_0_cal, relax_batches[-1]["atomic_nums"], coords_log[-1], max_steps=100)
+    relax_batches.append({
+        "atomic_nums": relax_batches[-1]["atomic_nums"],
         "relax_len": len(coords_log),
     })
 
-coords_log = coords_log + get_molecules.relax(sevennet_0_cal, relax_batches[-1]["atomic_nums"], coords_log[-1], max_steps=100)
-relax_batches.append({
-    "atomic_nums": relax_batches[-1]["atomic_nums"],
-    "relax_len": len(coords_log),
-})
+    relaxation = {
+        "frames": []
+    }
 
-relaxation = {
-    "frames": []
-}
+    curr_idx = 0
+    for relax_batch in relax_batches:
+        atomic_nums = relax_batch["atomic_nums"]
+        relax_len = relax_batch["relax_len"]
+        while curr_idx < relax_len:
+            coords = coords_log[curr_idx]
+            relaxation["frames"].append({
+                "atomic_nums": atomic_nums,
+                "coords": coords.tolist(),
+            })
+            curr_idx += 1
+    json.dump(relaxation, open("relaxation.json", "w"), separators=(',', ':'))
 
-curr_idx = 0
-for relax_batch in relax_batches:
-    atomic_nums = relax_batch["atomic_nums"]
-    relax_len = relax_batch["relax_len"]
-    while curr_idx < relax_len:
-        coords = coords_log[curr_idx]
-        relaxation["frames"].append({
-            "atomic_nums": atomic_nums,
-            "coords": coords.tolist(),
-        })
-        curr_idx += 1
-json.dump(relaxation, open("relaxation.json", "w"), separators=(',', ':'))
+if __name__ == "__main__":
+    create_single_chain()
